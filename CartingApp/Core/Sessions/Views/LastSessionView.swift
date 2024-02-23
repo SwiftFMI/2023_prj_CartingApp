31540185611c5351056fc7a2cdc59434161a798e
StoyanTinchev <st8tinchev@gmail.com>
2024-02-23 17:56:03 +0200

    Code refactoring + removal of unusual spacing

--- CartingApp/Core/Sessions/Views/LastSessionView.swift
+++ CartingApp/Core/Sessions/Views/LastSessionView.swift
@@ -13,61 +13,59 @@ struct LastSessionView: View {
     @State private var showAllSessions = false
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                VStack {
-                    if viewModel.isLoading {
-                        ProgressView()
-                    } else {
-                        ScrollView {
-                            VStack(alignment: .center, spacing: 10) {
-                                headerView
-                                Divider()
-                                if let lastSession = viewModel.lastSessionWithBestLap {
-                                    SessionEntryView(session: lastSession.session, bestLapTime: lastSession.bestTime)
-                                    Button("See all sessions") {
-                                        showAllSessions = true
-                                    }
-                                    .background(
-                                        NavigationLink(destination: AllSessionsView(viewModel: viewModel), isActive: $showAllSessions) {
-                                            EmptyView()
-                                        }
-                                            .hidden()
-                                    )
-                                } else {
-                                    Text("No sessions available")
+        ZStack {
+            VStack {
+                if viewModel.isLoading {
+                    ProgressView()
+                } else {
+                    ScrollView {
+                        VStack(alignment: .center, spacing: 10) {
+                            headerView
+                            Divider()
+                            if let lastSession = viewModel.lastSessionWithBestLap {
+                                SessionEntryView(session: lastSession.session, bestLapTime: lastSession.bestTime)
+                                Button("See all sessions") {
+                                    showAllSessions = true
                                 }
+                                .background(
+                                    NavigationLink(destination: AllSessionsView(viewModel: viewModel), isActive: $showAllSessions) {
+                                        EmptyView()
+                                    }
+                                        .hidden()
+                                )
+                            } else {
+                                Text("No sessions available")
                             }
                         }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .background(Color.white)
-                        .cornerRadius(15)
-                        .shadow(radius: 10)
-                        .padding()
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 10)
+                    .padding()
                 }
-                .onAppear {
-                    viewModel.fetchLastSessionWithBestLap()
-                }
-                .frame(height: 210)
             }
+            .onAppear {
+                viewModel.fetchLastSessionWithBestLap()
+            }
+            .frame(height: 210)
         }
     }
-        var headerView: some View{
-            HStack(alignment: .center) {
-                Image(systemName: "flag.checkered.2.crossed")
-                    .font(.title2)
-                    .frame(alignment: .center)
-                Text("Last Session")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .frame(alignment: .center)
-            }
-            .frame(maxWidth: .infinity)
-            .padding(.top, 10)
+    var headerView: some View{
+        HStack(alignment: .center) {
+            Image(systemName: "flag.checkered.2.crossed")
+                .font(.title2)
+                .frame(alignment: .center)
+            Text("Last Session")
+                .font(.title2)
+                .fontWeight(.bold)
+                .frame(alignment: .center)
         }
-
+        .frame(maxWidth: .infinity)
+        .padding(.top, 10)
     }
+    
+}
 
 //            VStack {
 //                if viewModel.isLoading {
